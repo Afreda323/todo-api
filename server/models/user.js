d75ec9ceeb26b0bb3f403820752f39c8b20bc18e
Afreda323 <anthonyfreda323@gmail.com>
2017-06-22 12:31:54 -0400

    POST Users route, JWT method

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = require("mongoose").Schema;
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
 //===========
 //   USER
 //===========
@@ -9,10 +12,51 @@ const userSchema = new Schema({
     minlength: 1,
     trim: true,
     required: true,
-    unique: true
-  }
+    unique: true,
+    validate: {
+      isAsync: true,
+      validator: validator.isEmail,
+      message: "{Value} is not a valid email"
+    }
+  },
+  password: {
+    type: String,
+    minlength: 6,
+    required: true
+  },
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
+userSchema.methods.toJSON = function() {
+  let user = this;
+  let userObj = user.toObject();
+  return _.pick(userObj, ['_id', 'email']);
+};
+userSchema.methods.generateAuthToken = function() {
+  let user = this;
+  const access = "auth";
+  const token = jwt
+    .sign({ _id: user._id.toHexString(), access }, "AYYY")
+    .toString();
+  user.tokens.push({
+    access,
+    token
+  });
 
+  return user.save().then(() => {
+    return token;
+  });
+};
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- server/server.js
+++ server/server.js
@@ -1,99 +1,24 @@
-require('./config');
-const _ = require('lodash')
+require("./config");
+const _ = require("lodash");
 const express = require("express");
 const bodyParser = require("body-parser");
 const { ObjectID } = require("mongodb");
 const mongoose = require("./db/mongoose");
 
-const Todo = require("./models/todo");
-const User = require("./models/user");
+//=============
+//  Route Import
+//=============
+const userRoute = require("./routes/user");
+const todoRoute = require("./routes/todos");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use("/users", userRoute);
+app.use("/todos", todoRoute);
 
-app.post("/todos", (req, res) => {
-  if (req.body.text) {
-    const todo = new Todo({
-      text: req.body.text
-    });
-    todo
-      .save()
-      .then(todo => res.json({ todo }))
-      .catch(e => res.status(400).send({ error: "Somehting went wrong" }));
-  } else {
-    res.status(400).send({ error: "Please send some text" });
-  }
-});
-
-app.get("/todos", (req, res) => {
-  Todo.find({}, { __v: 0 })
-    .then(todos => res.json({ todos }))
-    .catch(err => res.status(400).send({ err: "Smething went wrong" }));
-});
-
-app.get("/todos/:id", (req, res) => {
-  const id = req.params.id;
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).json({ error: "Invalid ID" });
-  }
-  Todo.findById(id)
-    .then(todo => {
-      if (!todo) {
-        return res.status(404).json({ error: "Todo doesnt exist" });
-      }
-      res.json({ todo });
-    })
-    .catch(err => res.status(400).json({ error: "There was a problem" }));
-});
-app.delete("/todos/:id", (req, res) => {
-  const id = req.params.id;
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).json({ error: "Invalid ID" });
-  }
-  Todo.findByIdAndRemove(id)
-    .then(todo => {
-      if (!todo) {
-        return res.status(404).json({ error: "Doesnt exist" });
-      }
-      res.json({ todo });
-    })
-    .catch(err => res.status(400).json({ error: "There was a problem" }));
-});
-
-app.patch("/todos/:id", (req, res) => {
-  const id = req.params.id;
-  const body = _.pick(req.body, ["text", "completed"]);
-  if (!ObjectID.isValid(id)) {
-    res.status(404).json({ error: "Invalid ID" });
-  }
-
-  if (_.isBoolean(body.completed) && body.completed) {
-    body.completedAt = new Date().getTime();
-  } else {
-    body.completed = false;
-    body.completedAt = null;
-  }
-
-  Todo.findByIdAndUpdate(
-    id,
-    {
-      $set: body
-    },
-    {
-      new: true
-    }
-  )
-    .then(todo => {
-      if (!todo) {
-        return res.status(404).json({ error: "Doesnt Exist" });
-      }
-      res.json({ todo });
-    })
-    .catch(e => res.status(400).json({ error: "Something went wrong" }));
-});
 app.listen(port, () => {
   console.log(`Up on Port:${port} brah`);
 });
